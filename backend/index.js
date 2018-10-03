e298f10bee376da46692af3d79ccfe85d3e53280
Maria Turuta <mariaturuta@gmail.com>
2018-10-03 11:04:06 +0300

    created response, implemented in front and back

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.get('/chat', (request, response, next) => {
   const question = request.query.question;
-  response.end(`Question: ${question}\nAnswer: ${chatbot.answer(question)}`);
+  console.log(`Question: ${question}\nAnswer: ${chatbot.answer(question)}`);
+  response.set('Access-Control-Allow-Origin', '*');
+  response.status(200).json(chatbot.answer(question));
 });
 
 app.listen(8080, '0.0.0.0');
