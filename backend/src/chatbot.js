52ae3d436327307864d2c0c4e1a4460c2d360fc5
Roman UNTILOV <kikmor97@gmail.com>
2018-11-14 10:08:20 +0200

    Tweaked some configs and formating

--- backend/src/chatbot.js
+++ backend/src/chatbot.js
@@ -1,15 +1,11 @@
-'use strict';
-
 module.exports = {
   /**
    * This answers just one question as a demo function
    * @param {string} question The question for which the chatbot should provide an answer
    */
   answer(question) {
-    if (question === 'Who are you?') {
-      return 'I AM CHATBOT!!!';
-    } else {
-      return `I don't understand your question.`;
-    }
+    if (question === 'Who are you?') return 'I AM CHATBOT!!!';
+
+    return "I don't understand your question.";
   },
 };
